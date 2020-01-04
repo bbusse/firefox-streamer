69638d116f16a72fb3f96e419801c4b836a3bcda
Björn Busse <bj.rn@baerlin.eu>
2020-01-04 07:18:54 +0100

    Provide URL via ENV

--- stream-screenshot.py
+++ stream-screenshot.py
@@ -2,9 +2,18 @@
 import time
 from selenium import webdriver
 from selenium.webdriver.firefox.options import Options
+import socket
 import subprocess
+import os
+import sys
 
-url = 'https://uhr.ptb.de/'
+url = os.environ.get('URL')
+
+if len(url) < 12:
+    print("Not a valid URL")
+    sys.exit(1)
+
+port = 6000
 log_path = '/tmp/geckodriver.log'
 img_path = '/tmp'
 options = Options()
@@ -14,6 +23,13 @@ browser = webdriver.Firefox(options=options, service_log_path=log_path)
 
 t0 = int(round(time.time() * 1000))
 n = 0
+
+def get_ip_address():
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.connect(("8.8.8.8", 80))
+    return s.getsockname()[0]
+
+ip = get_ip_address()
 
 gstreamer = subprocess.Popen([
     'gst-launch-1.0', '-v', '-e',
@@ -32,7 +48,7 @@ while True:
     filename = img_path + '/image_' + str(n).zfill(4) + '.png'
     browser.save_screenshot(filename)
     t1 = int(round(time.time() * 1000))
-    print(str(t1 - t0) + " ms")
+    print(ip + " [" + url + "] " + str(t1 - t0) + " ms")
     t0 = t1
 
     with open(filename, 'rb') as f:
